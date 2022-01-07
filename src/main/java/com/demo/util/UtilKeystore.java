41349c9f0a5af738fb8a3cb2471195163fb513ff
Elkin R <efrozo23@misena.edu.co>
2022-01-07 16:47:28 -0500

    se agrego el certificado al keystore

--- src/main/java/com/demo/util/UtilKeystore.java
+++ src/main/java/com/demo/util/UtilKeystore.java
@@ -1,27 +1,44 @@
 package com.demo.util;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
 import java.util.Base64;
 
 public class UtilKeystore {
-	
-	public static String createKeystore() throws Exception {
-		try (FileOutputStream fos = new FileOutputStream("newKeyStoreFileName.jks")){
+
+	public static String createKeystore(String pathCert,String pathKeystore,String host) throws Exception {
+		try (FileOutputStream fos = new FileOutputStream(pathKeystore)) {
 			System.out.println("keystore type:" + KeyStore.getDefaultType());
+			Certificate mycert = loadCertificate(pathCert);
 			KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
 			char[] pwdArray = "password".toCharArray();
 			ks.load(null, pwdArray);
+			ks.setCertificateEntry(host, mycert);
 			ks.store(fos, pwdArray);
-			File file = new File("newKeyStoreFileName.jks");
+			File file = new File(pathKeystore);
 			byte[] bytesFile = Files.readAllBytes(file.toPath());
 			return Base64.getEncoder().encodeToString(bytesFile);
 		} catch (Exception e) {
+			//cambiar por exception personalizada
 			throw new Exception(e.getMessage());
 		}
-		
+
+	}
+
+	public static Certificate loadCertificate(String pathCert) throws Exception {
+		try (InputStream certificateInputStream = new FileInputStream(pathCert)) {
+			CertificateFactory cf = CertificateFactory.getInstance("X.509");
+			return cf.generateCertificate(certificateInputStream);
+		} catch (Exception e) {
+			//cambiar por exception personalizada
+			throw new Exception(e.getMessage());
+		}
 	}
 
 }
--- src/test/java/com/demo/util/UtilKeystoreTest.java
+++ src/test/java/com/demo/util/UtilKeystoreTest.java
@@ -2,13 +2,24 @@ package com.demo.util;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class UtilKeystoreTest {
 	
+	private ClassLoader classLoad;
+	
+	@Before
+	public void setup() {
+		classLoad = UtilKeystoreTest.class.getClassLoader();
+	}
+	
 	@Test
 	public void createKeystoreWhenGenerateFile() throws Exception {
-		String result = UtilKeystore.createKeystore();
+		String pathCert = classLoad.getResource("mycert.cert").getPath();
+		String pathKeystore = classLoad.getResource("").getPath() + "mykeystore.jks";
+		System.out.println(pathKeystore);
+		String result = UtilKeystore.createKeystore(pathCert,pathKeystore,"myhost");
 		System.out.println("Resultado " + result);
 		assertNotNull(result);
 	}
